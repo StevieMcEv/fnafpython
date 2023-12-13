819997700633c9a77fb81d6cc3e55ad5e5d71d1c
QuandaleDingling <152216622+QuandaleDingling@users.noreply.github.com>
2023-12-12 19:11:36 -0500

    Update SirchiaPezzi_Fnaf.py
    Added new class

--- SirchiaPezzi_Fnaf.py
+++ SirchiaPezzi_Fnaf.py
@@ -8,9 +8,29 @@ class animatronic:
         self.hatelight = hatelight
         self.roomsentering = roomsentering
 
-freddy = animatronic("Freddy", 0.1, False, ["stage","dining","bathroom","easthall","easthallcorner","office"])
-gfreddy = animatronic("Golden Freddy", 0.05, False, ["partsservice","office"])
+freddy = animatronic("Freddy", 0.1, False, ["stage","dining","bathroom","easthall","easthallcorner","office",])
+gfreddy = animatronic("Golden Freddy", 0.05, False, ["partsservice","office",])
 bonnie = animatronic("Bonnie", 0.3, True, ["stage", "dining","partsservice","westhall","easthallcorner"] )
 chica = animatronic("Chica", 0.2, False, ["stage", "dining", "kitchen","easthall","easthallcorner"])
 foxy = animatronic("Foxy", 0.25, False, ["cove, westhall"])
 yoan = animatronic("Yoan", 0.001, ["gym", "westhallcorner, "])
+
+
+class power:
+    
+    def __init__(self, stored: int, consumption: int, poweroutage : bool, foxyhit: bool)
+        self.stored = stored
+        self.consumption = consumption
+        self.poweroutage = poweroutage
+        self.foxyhit = foxyhit
+
+mainbreaker = power(100, 0, False, False)
+deskfan = power(0, 1, [False, True], False)
+leftdoor = power(0, 1.5, [False, True], [False, True])
+rightdoor = power(0, 1.5, [False, True], [False, True])
+
+
+alive = True
+
+while alive == True
+    
