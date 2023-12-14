250c71a8821301f7f020d7e8662b0249be4a34fb
QuandaleDingling <152216622+QuandaleDingling@users.noreply.github.com>
2023-12-14 15:23:12 -0500

    Create Actions.py

---
+++ Actions.py
@@ -0,0 +1,16 @@
+from SirchiaPezzi_Fnaf import location
+
+action = input(" ")
+            if action == "a":
+
+            elif action == "d":
+
+            elif action == "s":
+                location()
+
+            elif action == "q":
+
+            elif action == "e":
+
+            else:
+                print("Invalid value")
