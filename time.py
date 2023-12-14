fbcdcba729b237a5bde2aaf30e3e24abf2d6e203
QuandaleDingling <152216622+QuandaleDingling@users.noreply.github.com>
2023-12-14 14:29:43 -0500

    Update time.py
    made better time announcement system

--- time.py
+++ time.py
@@ -2,5 +2,16 @@
 def timer():
         while True:
                 clock = clock + 1
-                print(clock)
                 time.sleep(1)
+                if clock == 60:
+                        print("It is 1AM")
+                if clock == 120:
+                        print("It is 2AM")
+                if clock == 180:
+                        print("It is 3AM")
+                if clock == 240:
+                        print("It is 4AM")
+                if clock == 300
+                        print("It is 5AM")
+                if clock == 360:
+                        print("It is 6AM")
